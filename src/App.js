98f4abd24c46d20bbefc7aa457bf22543c511ca9
SowjiKanth <sowjikanth@192.168.1.2>
2019-07-24 01:08:32 +0530

    Added the project for Zen3 React Frontend Challenge

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import JobCardTable from './JobCardTable';
+
+function App() {
+  
+  return (
+    <div className="App">
+        <JobCardTable />
+    </div>
+  );
+}
+
+export default App;
